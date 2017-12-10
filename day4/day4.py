9fc5bf71be602cb6927d59d2ba9cb4e1f33b5c2a
andrewtp23 <25349004+andrewtp23@users.noreply.github.com>
2017-12-09 19:23:31 -0500

    day4

---
+++ day4/day4.py
@@ -0,0 +1,70 @@
+from collections import Counter
+
+def day4(text):
+    f = open(text)
+    correct = 0
+    line = f.readline()
+    words = line.split()
+    l = 0
+    for x in words:
+        current = x
+        for g in words:
+            if g == current:
+                l +=1
+    if l == len(words):
+        correct += 1
+    print correct
+    while line:
+        line = f.readline()
+        words = line.split()
+        l = 0
+        for x in words:
+            current = x
+            for g in words:
+                if g == current:
+                    l += 1
+        if l == len(words):
+            correct += 1
+    print('test')
+    correct -= 1 # offset final read
+    print correct
+
+    f.close()
+
+def is_anagram(input1, input2):
+    return Counter(input1) == Counter(input2)
+
+def day4(text):
+    f = open(text)
+    correct = 0
+    line = f.readline()
+    words = line.split()
+    l = 0
+    for x in words:
+        current = x
+        for g in words:
+            if is_anagram(g, current):
+                l +=1
+    if l == len(words):
+        correct += 1
+    print correct
+    while line:
+        line = f.readline()
+        words = line.split()
+        l = 0
+        for x in words:
+            current = x
+            for g in words:
+                if is_anagram(g, current):
+                    l += 1
+        if l == len(words):
+            correct += 1
+    print('test')
+    correct -= 1 # offset final read
+    print correct
+
+    f.close()
+
+
+if __name__ == '__main__':
+    day4('day4input')
